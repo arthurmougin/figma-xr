02e4d8c2948d7c0868a4aa2b0842b24118b9230b
arthurM <arthur.mougin@live.fr>
2025-07-23 20:29:27 +0200

    this should fix the prod errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    readonly VITE_FIGMA_CLIENT_ID: string
-    readonly VITE_FIGMA_SECRET: string
-    // more env variables...
+	readonly VITE_APP_TITLE: string;
+	readonly VITE_ID: string;
+	readonly VITE_SECRET: string;
+	// more env variables...
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+	readonly env: ImportMetaEnv;
 }
